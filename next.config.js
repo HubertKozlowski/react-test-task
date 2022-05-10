f1b02f6ca8a7e8f0184d0c7565980ee872c8c3ab
HubertK <hubert.kozlowski@devapo.pl>
2022-05-10 16:34:03 +0200

    Next image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withImages = require("next-images");
 
 module.exports = withImages({
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages",
+  dynamicAssetPrefix: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   webpack(config, options) {
     return config;
   },
