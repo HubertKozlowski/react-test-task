ad2796a1d491ea22badd102f7dc2177932a17f32
HubertK <hubert.kozlowski@devapo.pl>
2022-05-10 20:37:54 +0200

    Links fixes

--- src/components/Navigation/NavigationLinks/index.tsx
+++ src/components/Navigation/NavigationLinks/index.tsx
@@ -1,17 +1,13 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
 
 const LINKS: { path: string; description: string }[] = [
-  { path: "/lorem-1", description: "Lorem" },
-  { path: "/lorem-2", description: "Lorem" },
-  { path: "/lorem-3", description: "Lorem" },
+  { path: "lorem-1", description: "Lorem" },
+  { path: "lorem-2", description: "Lorem" },
+  { path: "lorem-3", description: "Lorem" },
 ];
 
 export const NavigationLinks = (): JSX.Element => {
-  const { asPath } = useRouter();
-  console.log(`🚀 ~ file: index.tsx ~ line 13 ~ asPath`, asPath);
-
   return (
     <div className="flex font-bold">
       {LINKS.map(({ path, description }, index) => {
