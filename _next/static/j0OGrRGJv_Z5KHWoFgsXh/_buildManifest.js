144a5ecf4465f7b5959775e19caa8f5c35e78d5a
HubertKozlowski <40128575+HubertKozlowski@users.noreply.github.com>
2022-05-10 16:38:01 +0000

    Deploying to gh-pages from @ HubertKozlowski/react-test-task@94bf092e0e91dbbab7ed4e1b31e571ed430b85b2 🚀

--- _next/static/j0OGrRGJv_Z5KHWoFgsXh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-75237d29e7b147e0.js","static/chunks/pages/index-827eb1037e0e6d38.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
